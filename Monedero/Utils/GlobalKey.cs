77dbbf200ffb6493c7281f28c01146ce052af807
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-05-25 15:37:47 -0600

    10 - Resolucion para IOS Y Android

--- Monedero/Utils/GlobalKey.cs
+++ Monedero/Utils/GlobalKey.cs
@@ -8,8 +8,8 @@
         public static readonly string HOME = "HomePage";
         public static readonly string DETAILS = "DetailsPage";
         public static readonly string BALANCE = "Balace";
-        public static readonly string HOST = "https://monedero.tiendassmart.com:40499/index.php";//Test
-       // public static readonly string HOST = "https://monedero.tiendassmart.com:40500/index.php";//Production
+        // public static readonly string HOST = "https://monedero.tiendassmart.com:40099/index.php";
+        public static readonly string HOST = "https://10.40.10.9:40099/index.php";
         public const string END_POINT_GET_BALANCE = "/monederos/{no_tarjeta}/{apellido}";
 
     }
--- Monedero/Views/PopupPage.xaml.cs
+++ Monedero/Views/PopupPage.xaml.cs
@@ -16,5 +16,14 @@ public partial class PopupPage : BasePopupPage
        //FrameContainer.Margin = new Thickness(ScreenHelper.GetAdjustedSize(15), ScreenHelper.GetAdjustedSize(70));
         ImageFigure.HeightRequest = ScreenHelper.GetAdjustedSize(30);
         ImageFigure.WidthRequest = ScreenHelper.GetAdjustedSize(150);
+
+        if (Device.RuntimePlatform == Device.Android)
+        {
+            ImageFigure.WidthRequest = ScreenHelper.GetAdjustedSize(150);
+        }
+        if (Device.RuntimePlatform == Device.iOS)
+        {
+            ImageFigure.WidthRequest = ScreenHelper.GetAdjustedSize(175);
+        }
     }
 }
